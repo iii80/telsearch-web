4c73e4e78384ce06ae983c9b69bf894810a20382
long2ice <long2ice@gmail.com>
2022-06-19 01:41:42 +0800

    add search

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,51 +1,58 @@
 import React from "react";
-import {getSearchTips} from "../api";
-import {Link} from "react-router-dom";
+import { getSearchTips } from "../api";
+import { Link, useNavigate } from "react-router-dom";
+import SearchIcon from "../components/search-icon";
 
 function Index() {
-    const [input, setInput] = React.useState("");
-    const [tips, setTips] = React.useState([]);
-    const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value);
-        let data = await getSearchTips(e.target.value);
-        setTips(data);
-    }
-    const onBlur = (e:React.FocusEvent<HTMLInputElement>) => {
-        setTips([]);
-        // @ts-ignore
-        e.relatedTarget?.click()
-    }
-    return (
-        <div className="flex flex-col justify-center items-center pt-[10%]">
-            <h1 className="text-5xl font-bold mb-10">TelSearch</h1>
-            <div className="form-control w-1/3">
-                <div className="input-group w-full">
-                    <input type="text" placeholder="Search telegram channels here..."
-                           value={input}
-                           onChange={async (e) => await onChange(e)}
-                           onBlur={onBlur}
-                           className="input input-bordered w-full"/>
-                    <button className="btn btn-square">
-                        <Link to={'/search?keyword=' + input}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
-                            </svg>
-                        </Link>
-                    </button>
-                </div>
-                {
-                    tips &&
-                    <ul className="menu bg-base-100 mt-3 rounded-md">
-                        {tips.map((tip, index) => <li key={tip}><Link to={'/search?keyword=' + tip}>{tip}</Link></li>)}
-                    </ul>
-                }
-
-            </div>
-
+  const [input, setInput] = React.useState("");
+  const [tips, setTips] = React.useState([]);
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    let data = await getSearchTips(e.target.value);
+    setTips(data);
+  };
+  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setTips([]);
+    // @ts-ignore
+    e.relatedTarget?.click();
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col justify-center items-center pt-[10%]">
+      <h1 className="text-5xl font-bold mb-10">TelSearch</h1>
+      <div className="form-control w-1/3">
+        <div className="input-group w-full">
+          <input
+            type="text"
+            placeholder="Search telegram channels here..."
+            value={input}
+            onChange={async (e) => await onChange(e)}
+            onBlur={onBlur}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                navigate("/search?keyword=" + input);
+              }
+            }}
+            className="input input-bordered w-full"
+          />
+          <button className="btn btn-square">
+            <Link to={"/search?keyword=" + input}>
+              <SearchIcon />
+            </Link>
+          </button>
         </div>
-    );
+        {tips && (
+          <ul className="menu bg-base-100 mt-3 rounded-md">
+            {tips.map((tip, index) => (
+              <li key={tip}>
+                <Link to={"/search?keyword=" + tip}>{tip}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Index;
--- src/views/search.tsx
+++ src/views/search.tsx
@@ -1,41 +1,77 @@
-import {useSearchParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {getSearchResults} from "../api";
+import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getSearchResults } from "../api";
+import SearchIcon from "../components/search-icon";
 
 interface SearchResultItem {
-    id: number;
-    title: string;
-    count: number;
-    description: string;
-    language: string;
-    link: string;
-    photo: string;
-    create_time: Date;
-    update_time: Date;
-    full_photo: string;
+  id: number;
+  title: string;
+  count: number;
+  description: string;
+  language: string;
+  link: string;
+  photo: string;
+  create_time: Date;
+  update_time: Date;
+  full_photo: string;
 }
 
 interface SearchResult {
-    total: number;
-    data: Array<SearchResultItem>
+  total: number;
+  data: Array<SearchResultItem>;
 }
 
 function Search() {
-    const [searchParams] = useSearchParams();
-    const [results, setResults] = useState<SearchResult>();
-    useEffect(() => {
-        (async () => {
-            const keyword = searchParams.get("keyword") ?? "";
-            const results = await getSearchResults(keyword);
-            console.log(results)
-            setResults(results);
-        })();
-    }, [searchParams])
-    return (
-        <div>
-            {results && results.total}
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [results, setResults] = useState<SearchResult>();
+  const [input, setInput] = React.useState("");
+
+  useEffect(() => {
+    (async () => {
+      const keyword = searchParams.get("keyword") ?? "";
+      setInput(keyword);
+      const results = await getSearchResults(keyword);
+      setResults(results);
+    })();
+  }, [searchParams]);
+  return (
+    <div className="container mx-auto h-full">
+      <div className="flex w-full justify-center">
+        <h1 className="text-5xl font-bold pt-10">TelSearch</h1>
+      </div>
+      <div className="flex justify-center w-full py-10">
+        <div className="input-group w-[50%]">
+          <input
+            type="text"
+            placeholder="Search telegram channels here..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setSearchParams({ keyword: input });
+              }
+            }}
+            className="input input-bordered w-full"
+          />
+          <button
+            className="btn btn-square"
+            onClick={() => setSearchParams({ keyword: input })}
+          >
+            <SearchIcon />
+          </button>
         </div>
-    )
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        {results?.data?.map((item, index) => (
+          <div className="bg-gray-400 h-28 rounded p-2">
+            <div>{item.title}</div>
+            <div className="truncate">{item.description}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Search;
