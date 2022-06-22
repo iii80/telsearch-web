f5caede37dcc56b1aca0d0857f30a0bb6cb03b62
long2ice <long2ice@gmail.com>
2022-06-23 00:00:26 +0800

    add sign check

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,25 @@
 import axios from "axios";
+import { getRandomStr, get_sign } from "./sign";
 
-export default axios.create({
+const http = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
 });
+
+http.interceptors.request.use((config) => {
+  let timestamp = new Date().getTime().toString().substring(0, 10);
+  let nonce = getRandomStr(8);
+  let data;
+  if (config.method === "get" || config.method === "delete") {
+    data = config.params;
+  } else {
+    data = config.data;
+  }
+  // @ts-ignore
+  config.headers["timestamp"] = timestamp;
+  // @ts-ignore
+  config.headers["nonce"] = nonce;
+  // @ts-ignore
+  config.headers["sign"] = get_sign(data, timestamp, nonce);
+  return config;
+});
+export default http;
