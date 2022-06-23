adb1dc3ecc43c8bb106035b94863188151fcbb32
long2ice <long2ice@gmail.com>
2022-06-23 20:15:19 +0800

    add modal in search page

--- src/views/searchResults.tsx
+++ src/views/searchResults.tsx
@@ -38,11 +38,55 @@ function SearchResults() {
       <SearchComponent className="lg:py-10 py-5 lg:px-[25%] px-[4%]" />
       <div className="grid lg:grid-cols-4 gap-4">
         {results?.data?.map((item) => (
-          <div
-            className="hover:cursor-pointer flex place-content-center"
-            onClick={async () => await onClickItem(item)}
+          <label
+            htmlFor={"modal-" + item.id}
+            className="hover:cursor-pointer flex place-content-center modal-button"
             key={item.id}
           >
+            <input
+              type="checkbox"
+              id={"modal-" + item.id}
+              className="modal-toggle"
+            />
+            <div className="modal">
+              <div className="modal-box">
+                <div className="flex flex-row space-x-2">
+                  <div className="avatar">
+                    <div className="w-20 h-20 rounded-md">
+                      <img src={item.full_photo} alt={item.title} />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">
+                      <Highlighter
+                        searchWords={[searchParams.get("keyword") ?? ""]}
+                        textToHighlight={item.title}
+                        highlightClassName={"bg-fuchsia-300 text-fuchsia-900"}
+                      />
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      {item.count} members | {item.update_time.split("T")[0]}
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      <Highlighter
+                        searchWords={[searchParams.get("keyword") ?? ""]}
+                        textToHighlight={item.description}
+                        highlightClassName={"bg-fuchsia-300 text-fuchsia-900"}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex w-full mt-2 ">
+                  <label
+                    htmlFor={"modal-" + item.id}
+                    className="btn ml-auto"
+                    onClick={async () => await onClickItem(item)}
+                  >
+                    Open
+                  </label>
+                </div>
+              </div>
+            </div>
             <div className="rounded bg-base-100 shadow-xl flex flex-col justify-center p-3 lg:w-full w-11/12">
               <div className="flex flex-row space-x-2">
                 <div className="avatar">
@@ -71,7 +115,7 @@ function SearchResults() {
                 </div>
               </div>
             </div>
-          </div>
+          </label>
         ))}
       </div>
       <div className="flex w-full justify-center lg:py-20 py-5">
